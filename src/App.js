70744097bfdca5e7cba42e06da9b434aaef6207e
neo@zillinks.com <keunhey0107@gmail.com>
2023-10-18 21:45:34 +0900

    Update:socket-io 서버 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import "./App.css";
+import './App.css';
+import socket from './server';
 
 function App() {
   return (
     <div>
-      <div className="App"></div>
+      <div className='App'></div>
     </div>
   );
 }
